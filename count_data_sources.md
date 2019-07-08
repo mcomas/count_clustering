499f49a126f57ee512e5311d7880920d670f601d
mcomas <mar.comas@udg.edu>
2019-07-08 08:54:53 +0200

    Examples

---
+++ count_data_sources.md
@@ -0,0 +1,14 @@
+
+
+# There are the following packages which can contain multivariate count data.
+
+mvabund
+vegan
+labdsv
+
+
+# Count data for text
+[](https://datascience.stackexchange.com/questions/1079/data-sets-for-evaluating-text-retrieval-quality)
+
+mmm
+
---
+++ text_example_01/download_data.R
@@ -0,0 +1,7 @@
+# http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/lyrl2004_rcv1v2_README.htm
+download.file('http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a01-list-of-topics/rcv1.topics.txt',
+              'text_example_01/rcv1.topics.txt')
+download.file('http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a02-orig-topics-hierarchy/rcv1.topics.hier.orig',
+              'text_example_01/rcv1.topics.hier.orig')
+download.file('http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a03-expanded-topics-hierarchy/rcv1.topics.hier.expanded',
+              'text_example_01/rcv1.topics.hier.expanded')
